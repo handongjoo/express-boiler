53bc7ef34c8db7e8acc7b894c3deee8725306d1f
handongjoo <dongjooyoung@naver.com>
2024-01-18 16:50:09 +0900

    파일 구조 생성 및 ejs 템플릿 엔진 렌더링 테스트

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const userRouter = require('./routers/userRouter');
 const app = express();
+const path = require('path');
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+// 정적파일 경로
+app.use(express.static('public'));
 
+// 라우터 목록
 app.use('/users', userRouter);
+
+// ejs 엔진 사용
+app.set('view engine', 'ejs')
+// ejs 템플릿 위치
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
 
 app.listen(port, () => {
     console.log(`app listen port : ${port}`)
